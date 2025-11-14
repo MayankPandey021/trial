e2ed4fb4563ab74ec567b819ac9210996111a63d
MayankPandey021 <maxgaucho021@gmail.com>
2025-11-14 12:31:17 +0530

    change

--- prac.java
+++ prac.java
@@ -2,4 +2,5 @@ public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("My name is ");
     System.out.println("");
+    System.out.println("Prac");
 }
