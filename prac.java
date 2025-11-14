5fdfec1ff620a4f0a3cd0b535ae25175e59745c1
MayankPandey021 <maxgaucho021@gmail.com>
2025-11-14 12:32:18 +0530

    change

--- prac.java
+++ prac.java
@@ -3,4 +3,6 @@ public static void main(String[] args) {
     System.out.println("My name is ");
     System.out.println("");
     System.out.println("Prac");
+    System.out.println("My name is ");
+    System.out.println("");
 }
